427a00f6b10f6a832d09a7f8831982b545910a76
Mitsuhiro Komuro <mitsuhiro.komuro@gmail.com>
2020-07-25 08:57:09 +0900

    Add event and state class for todo list
    Update pubspec for BLoC pattern

--- lib/models/todo.dart
+++ lib/models/todo.dart
@@ -1,14 +1,17 @@
 class Todo {
+  int _id = 0;
   String _title = '';
   String _details = '';
   bool _isDone = false;
   bool _isStared = false;
 
-  Todo(this._title, this._details,
+  Todo(this._id, this._title, this._details,
       {bool isDone = false, bool isStared = false}) {
     this._isDone = isDone;
     this._isStared = _isStared;
   }
+
+  int get id => this._id;
 
   String get title => this._title;
 
